d6c44334e138c950e17275e737f129d807a66d28
suhani <suhanik86@gmail.com>
2019-12-22 14:40:30 +0530

    another change to bike file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -9,7 +9,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
-    Adding a comment here for timepass 
+    Adding a comment here for timepass
+    Another change 
     Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
